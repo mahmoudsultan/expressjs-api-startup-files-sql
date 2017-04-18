3a3d1640cca9895aa6587b6556552eb5f9363e2c
Mahmoudsultan <mahmoud.m.sultan36@gmail.com>
2017-04-18 21:38:45 +0200

    Heroku deployment database connection added

--- models/main.js
+++ models/main.js
@@ -1,8 +1,25 @@
 const Sequelize = require('sequelize'),
     config = require('./../config');
 
-var connection = new Sequelize(config.database.name, config.database.username,
-    config.database.password, config.database.config);
+// this is for deployment in heroku
+if (process.env.DATABASE_URL) {
+
+    var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+    var connection = new Sequelize(match[5], match[1], match[2], {
+        dialect: 'postgres', // postgres is free in heroku
+        protocol: 'postgres',
+        port: match[4],
+        host: match[3],
+        logging: false,
+        dialectOptions: {
+            ssl: true
+        }
+    });
+    
+} else {
+    var connection = new Sequelize(config.database.name, config.database.username,
+        config.database.password, config.database.config);
+}
 
 var Movies = connection.import(__dirname + '/movies.js');
 
